8fb28807355d5e80dcada3ed1a453a621db135b2
Ahmad Ali Karim <98990857+EmpAhmadK@users.noreply.github.com>
2022-04-13 19:18:50 +0800

    a

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -48,51 +48,59 @@ export const Keyboard = ({ onChar, onDelete, onEnter, guesses }: Props) => {
   }, [onEnter, onDelete, onChar])
 
   return (
-    <div>
-      <div className="flex justify-center mb-1">
-        {ORTHOGRAPHY.slice(0, Math.floor(ORTHOGRAPHY.length * 0.4)).map(
-          (char) => (
-            <Key
-              key={char}
-              value={char}
-              onClick={onClick}
-              status={charStatuses[char]}
-            />
-          )
-        )}
-      </div>
-      <div className="flex justify-center mb-1">
-        {ORTHOGRAPHY.slice(
-          Math.floor(ORTHOGRAPHY.length * 0.4),
-          Math.floor(ORTHOGRAPHY.length * 0.7)
-        ).map((char) => (
-          <Key
-            key={char}
-            value={char}
-            onClick={onClick}
-            status={charStatuses[char]}
-          />
-        ))}
-      </div>
-      <div className="flex justify-center">
-        <Key key="enterKey" width={65.4} value="ENTER" onClick={onClick}>
-          {t('enterKey')}
-        </Key>
-        {ORTHOGRAPHY.slice(
-          Math.floor(ORTHOGRAPHY.length * 0.7),
-          ORTHOGRAPHY.length
-        ).map((char) => (
-          <Key
-            key={char}
-            value={char}
-            onClick={onClick}
-            status={charStatuses[char]}
-          />
-        ))}
-        <Key key="deleteKey" width={65.4} value="DELETE" onClick={onClick}>
-          {t('deleteKey')}
-        </Key>
-      </div>
+    <div id="keyboard-cont">
+        <div class="first-row">
+            <button class="keyboard-button">ج</button>
+            <button class="keyboard-button">ح</button>
+            <button class="keyboard-button">خ</button>
+            <button class="keyboard-button">ه</button>
+            <button class="keyboard-button">ع</button>
+            <button class="keyboard-button">غ</button>
+            <button class="keyboard-button">ف</button>
+            <button class="keyboard-button">ق</button>
+            <button class="keyboard-button">ث</button>
+            <button class="keyboard-button">ص</button>
+            <button class="keyboard-button">ض</button>
+        </div>
+        <div class="second-row">
+            <button class="keyboard-button">ط</button>
+            <button class="keyboard-button">ک</button>
+            <button class="keyboard-button">م</button>
+            <button class="keyboard-button">ن</button>
+            <button class="keyboard-button">ت</button>
+            <button class="keyboard-button">ا</button>
+            <button class="keyboard-button">ل</button>
+            <button class="keyboard-button">ب</button>
+            <button class="keyboard-button">ي</button>
+            <button class="keyboard-button">س</button>
+            <button class="keyboard-button">ص</button>
+        </div>
+        <div class="third-row">
+            <button class="keyboard-button">ظ</button>
+            <button class="keyboard-button">ز</button>
+            <button class="keyboard-button">و</button>
+            <button class="keyboard-button">ة</button>
+            <button class="keyboard-button">ى</button>
+            <button class="keyboard-button">ڠ</button>
+            <button class="keyboard-button">ر</button>
+            <button class="keyboard-button">ۏ</button>
+            <button class="keyboard-button">ڤ</button>
+            <button class="keyboard-button">ڽ</button>
+            <button class="keyboard-button">ج</button>
+        </div>
+        <div class="fourth-row">
+            <button class="keyboard-button keyboard-special">↵</button>
+            <button class="keyboard-button">ݢ</button>
+            <button class="keyboard-button">د</button>
+            <button class="keyboard-button">٢</button>
+            <button class="keyboard-button">ئ</button>
+            <button class="keyboard-button">إ</button>
+            <button class="keyboard-button">أ</button>
+            <button class="keyboard-button">ٴ</button>
+            <button class="keyboard-button">ء</button>
+            <button class="keyboard-button">ذ</button>
+            <button class="keyboard-button keyboard-special">⌫</button>
+        </div>
     </div>
   )
 }
